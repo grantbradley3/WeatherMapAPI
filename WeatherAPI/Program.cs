a7c8c7e2c8d720720efbc047f2404b370bda3d69
Grant Bradley <grantbradleytiger13@gmail.com>
2023-05-19 15:24:38 -0400

    Initial Commit

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace WeatherAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Enter your zipcode");
+            var zipcode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipcode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+        }
+    }
+}
